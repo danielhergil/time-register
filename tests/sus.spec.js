ef78cb40fda8b3335870af31d7835331aceff0e1
danielhergil <daniel.hergil@gmail.com>
2025-02-16 22:22:20 +0100

    updated urls

--- tests/sus.spec.js
+++ tests/sus.spec.js
@@ -1,5 +1,5 @@
-require('dotenv').config()
-const { test } = require('@playwright/test');
+require("dotenv").config();
+const { test } = require("@playwright/test");
 
 // Generate a random number between min (inclusive) and max (exclusive)
 function getRandomNumber(min, max) {
@@ -7,11 +7,11 @@ function getRandomNumber(min, max) {
 }
 
 // Define the range
-const minLat = 40.43500;
-const maxLat = 40.43530;
+const minLat = 40.435;
+const maxLat = 40.4353;
 
-const minLon = -3.30160;
-const maxLon = -3.30190;
+const minLon = -3.3016;
+const maxLon = -3.3019;
 
 // Generate the random number
 const randomNumberLatitude = getRandomNumber(minLat, maxLat);
@@ -21,62 +21,75 @@ console.log(randomNumberLatitude.toFixed(5)); // Output with 5 decimal places
 console.log(randomNumberLongitude.toFixed(5)); // Output with 5 decimal places
 
 test.use({
-    geolocation: {
-      latitude: parseFloat(randomNumberLatitude.toFixed(5)),
-      longitude: parseFloat(randomNumberLongitude.toFixed(5)),
-    },
-    permissions: ['geolocation'],
+  geolocation: {
+    latitude: parseFloat(randomNumberLatitude.toFixed(5)),
+    longitude: parseFloat(randomNumberLongitude.toFixed(5)),
+  },
+  permissions: ["geolocation"],
 });
 
+test("time register in", { tag: "@entrada" }, async ({ page }) => {
+  await page.goto(
+    "https://empleadovillalbilla.eadministracion.es/PortalEmpleadoV2/Menus/wfrBienvenida.aspx?param=MjgmMTcy&BORDE=S"
+  );
+  await page.locator('xpath=//div[@id="btnSession_CD"]').click();
 
-test('time register in', {tag: '@entrada',}, async ({ page }) => {
-    
-    await page.goto('https://empleadovillalbilla.eadministracion.es/PortalEmpleado/Menus/wfrBienvenida.aspx?param=MjgmMTcy');
-    await page.locator('xpath=//div[@id="btnSession_CD"]').click();
+  await page
+    .locator('xpath=//input[@id="master_pnlContenido_txtLogin_I"]')
+    .fill(process.env.POL_SUS_USER);
+  await page
+    .locator('xpath=//input[@id="master_pnlContenido_txtPassword_I"]')
+    .fill(process.env.POL_SUS_PASS);
 
-    await page.locator('xpath=//input[@id="master_pnlContenido_txtLogin_I"]').fill(process.env.POL_SUS_USER)
-    await page.locator('xpath=//input[@id="master_pnlContenido_txtPassword_I"]').fill(process.env.POL_SUS_PASS)
-    
-    await page.locator('xpath=//div[@id="master_pnlContenido_btnAceptar_CD"]').click();
+  await page.locator('xpath=//div[@id="master_pnlContenido_btnAceptar_CD"]').click();
 
-    await page.goto("https://empleadovillalbilla.eadministracion.es/PortalEmpleado/Personal/wfrFichaje.aspx")
+  await page.goto(
+    "https://empleadovillalbilla.eadministracion.es/PortalEmpleadoV2/Personal/wfrFichaje.aspx"
+  );
 
-    await page.waitForTimeout(5000)
+  await page.waitForTimeout(5000);
 
-    await page.locator('xpath=//input[@id="master_pnlContenido_cmbMotivoFichaje_I"]').click()
+  await page.locator('xpath=//input[@id="master_pnlContenido_cmbMotivoFichaje_I"]').click();
 
-    await page.waitForSelector('xpath=//td[normalize-space(text())="E - Entrada"]');
-    await page.locator('xpath=//td[normalize-space(text())="E - Entrada"]').click();
+  await page.waitForSelector('xpath=//td[normalize-space(text())="E - Entrada"]');
+  await page.locator('xpath=//td[normalize-space(text())="E - Entrada"]').click();
 
-    await page.waitForTimeout(2000)
+  await page.waitForTimeout(2000);
 
-    await page.locator('xpath=//div[@id="master_pnlContenido_btnFichar"]').click();
-    await page.waitForTimeout(10000)
-    await page.screenshot({ path: 'screenshot.png' });
-  });
+  await page.locator('xpath=//div[@id="master_pnlContenido_btnFichar"]').click();
+  await page.waitForTimeout(10000);
+  await page.screenshot({ path: "screenshot.png" });
+});
 
-  test('time register exit', {tag: '@salida',}, async ({ page }) => {
-    
-    await page.goto('https://empleadovillalbilla.eadministracion.es/PortalEmpleado/Menus/wfrBienvenida.aspx?param=MjgmMTcy');
-    await page.locator('xpath=//div[@id="btnSession_CD"]').click();
+test("time register exit", { tag: "@salida" }, async ({ page }) => {
+  await page.goto(
+    "https://empleadovillalbilla.eadministracion.es/PortalEmpleadoV2/Menus/wfrBienvenida.aspx?param=MjgmMTcy&BORDE=S"
+  );
+  await page.locator('xpath=//div[@id="btnSession_CD"]').click();
 
-    await page.locator('xpath=//input[@id="master_pnlContenido_txtLogin_I"]').fill(process.env.POL_SUS_USER)
-    await page.locator('xpath=//input[@id="master_pnlContenido_txtPassword_I"]').fill(process.env.POL_SUS_PASS)
-    
-    await page.locator('xpath=//div[@id="master_pnlContenido_btnAceptar_CD"]').click();
+  await page
+    .locator('xpath=//input[@id="master_pnlContenido_txtLogin_I"]')
+    .fill(process.env.POL_SUS_USER);
+  await page
+    .locator('xpath=//input[@id="master_pnlContenido_txtPassword_I"]')
+    .fill(process.env.POL_SUS_PASS);
 
-    await page.goto("https://empleadovillalbilla.eadministracion.es/PortalEmpleado/Personal/wfrFichaje.aspx")
+  await page.locator('xpath=//div[@id="master_pnlContenido_btnAceptar_CD"]').click();
 
-    await page.waitForTimeout(5000)
+  await page.goto(
+    "https://empleadovillalbilla.eadministracion.es/PortalEmpleadoV2/Personal/wfrFichaje.aspx"
+  );
 
-    await page.locator('xpath=//input[@id="master_pnlContenido_cmbMotivoFichaje_I"]').click()
-    
-    await page.waitForSelector('xpath=//td[normalize-space(text())="S - Salida"]');
-    await page.locator('xpath=//td[normalize-space(text())="S - Salida"]').click();
+  await page.waitForTimeout(5000);
 
-    await page.waitForTimeout(2000)
+  await page.locator('xpath=//input[@id="master_pnlContenido_cmbMotivoFichaje_I"]').click();
 
-    await page.locator('xpath=//div[@id="master_pnlContenido_btnFichar"]').click();
-    await page.waitForTimeout(10000)
-    await page.screenshot({ path: 'screenshot.png' });
-  });
+  await page.waitForSelector('xpath=//td[normalize-space(text())="S - Salida"]');
+  await page.locator('xpath=//td[normalize-space(text())="S - Salida"]').click();
+
+  await page.waitForTimeout(2000);
+
+  await page.locator('xpath=//div[@id="master_pnlContenido_btnFichar"]').click();
+  await page.waitForTimeout(10000);
+  await page.screenshot({ path: "screenshot.png" });
+});
